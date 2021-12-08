8f35f26ffd6b5b211a060aa739326f13a1a78ea3
takiyu <takiyu1025.txu.development@gmail.com>
2021-12-08 23:48:50 +0000

    Add NotoColorEmojiNerd generation.

--- convert_noto_emoji.py
+++ convert_noto_emoji.py
@@ -5,8 +5,8 @@ fonts = [
     "./src/Hack Regular Nerd Font Complete Mono Windows Compatible.ttf",
 #     "./src/DejaVuSansMono.ttf",
 #     "./src/iconsfordevs.ttf",
-    "./src/Noto Mono for Powerline.ttf",
+#     "./src/Noto Mono for Powerline.ttf",
 #     "./src/NotoSansCJKjp-Regular.ttf",
 ]
 merged_font = Merger().merge(fonts)
-merged_font.save('./src/converted_noto_emoji.ttf')
+merged_font.save('./NotoColorEmojiNerd.ttf')
