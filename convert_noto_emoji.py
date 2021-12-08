3f366bf92d65d1ae2b447fcbbc5234a103e5083c
takiyu <takiyu1025.txu.development@gmail.com>
2021-12-08 14:50:25 +0000

    Merge NotoColorEmoji and Nerd

---
+++ convert_noto_emoji.py
@@ -0,0 +1,12 @@
+from custom_fonttools.merge import Merger
+
+fonts = [
+    "./src/NotoColorEmoji_WindowsCompatible.ttf",
+    "./src/Hack Regular Nerd Font Complete Mono Windows Compatible.ttf",
+#     "./src/DejaVuSansMono.ttf",
+#     "./src/iconsfordevs.ttf",
+    "./src/Noto Mono for Powerline.ttf",
+#     "./src/NotoSansCJKjp-Regular.ttf",
+]
+merged_font = Merger().merge(fonts)
+merged_font.save('./src/converted_noto_emoji.ttf')
